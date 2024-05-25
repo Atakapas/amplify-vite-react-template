60dadfe3f76e0e5f1a6739e58ff82378bed535a5
Donald Mullen <myzenlife42@gmail.com>
2024-05-25 10:05:48 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,7 @@ import { generateClient } from "aws-amplify/data";
 const client = generateClient<Schema>();
 
 function App() {
-
-  
- 
-  const [todos, setTodos] = useState<Array<Schema["MedMarOne TO Do"]["type"]>>([]);
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -17,30 +14,32 @@ function App() {
   }, []);
 
   function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Don's Todo Content") });
+    client.models.Todo.create({ content: window.prompt("Don's To Do List") });
   }
- function deleteTodo(id: string) {
+  
+  function deleteTodo(id: string) {
     client.models.Todo.delete({ id })
-  }    return (
+  }
+  return (
     <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
+      <h1>Don's To Dos</h1>
+      <button onClick={createTodo}>Add a To Do</button>
       <ul>
         {todos.map((todo) => (
           <li 
-            onClick={() => deleteTodo(todo.id)}            
-            key={todo.id}>{todo.content}
-          </li>
+                      onClick={() => deleteTodo(todo.id)}
+            key={todo.id}>{todo.content}</li>
         ))}
       </ul>
       <div>
-        🥳 App successfully hosted. MedMarOne under construction.
-        <br />        <a href="https://youtube.com">
-          Go To YouTube
+        🥳 App successfully hosted. Try creating a Don to do.
+        <br />
+        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+          Review next step of this tutorial.
         </a>
       </div>
     </main>
   );
 }
 
-export default App;
+export default App;gh
