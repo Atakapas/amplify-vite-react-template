f63d1a48c87e32d0439586f09da562abf88b2e19
Donald Mullen <myzenlife42@gmail.com>
2024-05-24 20:57:58 -0500

    Update App.tsx
    back to original copy

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
+import type { Schema } from "../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
+
+const client = generateClient<Schema>();
+
 function App() {
-  // ...
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+  useEffect(() => {
+    client.models.Todo.observeQuery().subscribe({
+      next: (data) => setTodos([...data.items]),
+    });
+  }, []);
+
+  function createTodo() {
+    client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
   return (
@@ -9,19 +22,19 @@ function App() {
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
-        {todos.map(todo => <li
-          onClick={() => deleteTodo(todo.id)}
-          key={todo.id}>
-          {todo.content}
-        </li>)}
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.content}</li>
+        ))}
       </ul>
       <div>
-        🥳 App successfully hosted. Try creating a Don To Do .
+        🥳 App successfully hosted. Try creating a new todo.
         <br />
-
-          <button onClick={signOut}>Sign out</button>
-      
+        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+          Review next step of this tutorial.
+        </a>
+      </div>
     </main>
+  );
+}
 
-    
-      )
+export default App;
